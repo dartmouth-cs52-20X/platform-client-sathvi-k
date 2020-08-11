b0a7c7fed40ca5244b074e2a16d5840be657e8e4
Sathvi <47374355+sathvi-k@users.noreply.github.com>
2020-08-11 00:14:25 -0400

    fixing heroku build failure pt 2

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../style.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './navbar';
-import NewPost from './newPost';
+import NewPost from './newpost';
 import Post from './post';
 import Posts from './posts';
 import Fallback from './fallback';
