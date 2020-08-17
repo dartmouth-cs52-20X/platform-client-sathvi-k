29cf41ddddc2574ac241ce2b3bc5a328d577b9ec
Sathvi <47374355+sathvi-k@users.noreply.github.com>
2020-08-17 12:42:45 -0400

    changing root_url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'https://sathvi-lab5-platform.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://sathvi-lab5-platform.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 // const ROOT_URL = 'https://platform.cs52.me/api';
 // const API_KEY = '?key=skorandla';
 
